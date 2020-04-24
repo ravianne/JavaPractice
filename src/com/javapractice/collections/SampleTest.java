808fd65a4af2cbd52a5a0414cf0478542c85225e
ravianne <64114920+ravianne@users.noreply.github.com>
2020-04-24 11:37:28 +0530

    Initial Push

---
+++ src/com/javapractice/collections/SampleTest.java
@@ -0,0 +1,10 @@
+package com.javapractice.collections;
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Testing");
+	}
+
+}
